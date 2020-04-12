90627fc9d21b4cf4590f7d0e58d9adbc4708021d
Brett Andrews <brett.j.andrews@gmail.com>
2020-04-12 07:25:10 -0700

    fix: add link to civilization.com

--- packages/ui/src/App.js
+++ packages/ui/src/App.js
@@ -34,7 +34,7 @@ function App() {
         }
 
         <h2>What is this?</h2>
-        <p>Civilization 6 comes with a game mode called Play by Cloud that lets you play with friends without needing to be online at the same time. You simply take your turn when it suits you and the game state is saved to the cloud. This service notifies players who's turn it is via a Discord channel.</p>
+        <p><a href="https://civilization.com/" target="_blank">Civilization 6</a> comes with a game mode called Play by Cloud that lets you play with friends without needing to be online at the same time. You simply take your turn when it suits you and the game state is saved to the cloud. This service notifies players who's turn it is via a Discord channel.</p>
         <h2>How does it work?</h2>
         <p>Civilization 6 comes with a Webhook setting. When you set this, the game will send a request to that Webhook with information on the Game Name, the Turn Number, and the name of the player whose turn it is. This service takes that data and transforms it into something Discord can understand, and then forwards the request to the Discord Webhook you provide.</p>
 
