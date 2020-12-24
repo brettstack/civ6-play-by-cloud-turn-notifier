1907211b352977d6cbd06571d4bae4380f1f7ae3
Brett <brett@vendia.net>
2020-12-24 14:37:18 +1000

    fix(ui): respect user preference for dark/light modoe

--- packages/ui/src/App.js
+++ packages/ui/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from 'axios'
-// import useMediaQuery from '@material-ui/core/useMediaQuery'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import {
@@ -20,7 +20,7 @@ const {
 axios.defaults.baseURL = REACT_APP_ApiEndpoint
 
 function App() {
-  const prefersDarkMode = true //useMediaQuery('(prefers-color-scheme: dark)');
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
   const theme = React.useMemo(
     () =>
       createMuiTheme({
