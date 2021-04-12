133c78db20aee5790eff6dedd9458a99ab097130
Brett Andrews <brett.j.andrews@gmail.com>
2021-04-12 20:34:47 +1000

    fix: fix serverless.yaml

--- packages/api/dynamodb-init.js
+++ packages/api/dynamodb-init.js
@@ -2,11 +2,20 @@ import AWS from 'aws-sdk'
 import DynamoDB from 'aws-sdk/clients/dynamodb'
 import { Table } from 'dynamodb-toolbox'
 
+// TODO: v3 https://github.com/deeheber/note-service/pull/4/files
+// const { fromIni } = require("@aws-sdk/credential-provider-ini");
+
 const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, NODE_ENV } = process.env
 
+// For local development, get creds from ~/.aws/credentials
+// Alternatively, set AWS_PROFILE env var
 if (NODE_ENV !== 'test' && (!AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY)) {
   const credentials = new AWS.SharedIniFileCredentials({ profile: 'civ6_dev' })
   AWS.config.credentials = credentials
+  // TODO: V3
+  // const s3Client = new S3.S3Client({
+  //   credentials: fromIni({ profile: 'civ6_dev' })
+  // });
 }
 
 const dynamoDbConfig = {
